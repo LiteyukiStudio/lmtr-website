9993e2d2b62c9be290d8b01fed004a26f852b876
snowy <snowykami@outlook.com>
2024-07-31 10:07:15 +0800

    :zap: 添加来源和版权解释

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import {createRouter, createWebHashHistory} from "vue-router"
 import Home from "../views/Home.vue"
 import Surroundings from '../views/Surroundings.vue'
 import Join from '../views/Join.vue'
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory("/"),
+    history: createWebHashHistory("#"),
     routes
 })
 
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -6,7 +6,7 @@ import map from "../assets/system-plan.png"
 <div class="content" id="content-map">
     <h2>LMTR System Plan</h2>
     <img class="map" :src="map" alt="system-plan">
-    <p>该线路图使用<a href="https://railmapgen.github.io/rmp/">地铁线路图绘制器绘制</a>，图中所用素材部分为<a href="https://mtr.com.hk/">港铁公司</a>所设计，均为非商用</p>
+    <p style="text-align: center">该线路图使用<a href="https://railmapgen.github.io/rmp/">地铁线路图绘制器</a>绘制，图中所用素材部分为<a href="https://mtr.com.hk/">港铁公司</a>所设计，均为非商用</p>
 </div>
 </template>
 
